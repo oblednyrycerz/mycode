d16d8bde274ba972ebf8eb815fab592f43f0e30a
oblednyrycerz <peter@profundus.us>
2021-09-21 15:14:00 +0000

    another updates

---
+++ advlist/animal-list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# create a list called list1
+list1 = ["Fox", "Fly", "Ant", "Bee", "Cod", "Cat", "Dog", "Yak", "Cow", "Hen", "Koi", "Hog", "Jay", "Kit"]
+# display list1
+print(list1)
+
