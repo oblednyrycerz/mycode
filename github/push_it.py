f09a05e623660971eb4f2f9cfb7ed54b6a469396
oblednyrycerz <peter@profundus.us>
2021-09-22 16:09:08 +0000

    Lab 24-25

--- github/push_it.py
+++ github/push_it.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
-cd ~/mycode
-git add *
-git commit -m "Looping techniques"
-git push origin main
+'''
+   Date: September 22, 2021
+   Subject: push_it.py
+push_it.py is a simple process to knock out a "git push" to GitHub. 
+'''
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 
+import os
+
+commit_message    = input('Commit Comment: ')
+#working_directory = 'cd ~/mycode'
+working_directory = '/home/student/mycode'
+git_add           = 'git add *'
+git_commit        = 'git commit -m "' + commit_message + '"' 
+git_push          = 'git push origin'
+#os.system(working_directory)
+os.chdir(working_directory)
+os.system(git_add)
+os.system(git_commit)
+os.system(git_commit)
+os.system(git_push)
+
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
