fff0b4eba00eba56048cd7faa853cce9eb574741
oblednyrycerz <peter@profundus.us>
2021-09-22 17:41:18 +0000

    Testing pushit

--- github/push_it_MM.py
+++ github/push_it_MM.py
@@ -1,4 +1,4 @@
-!/usr/bin/env python3
+#!/usr/bin/env python3
 '''
    Date: September 21, 2021
    From: Mark Mollere, mmollere@alta3.com
@@ -11,35 +11,32 @@ critcal thinking for the next iteration...
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 
 import getopt
 import os
-import sys 
-
-argumentList = sys.argv[1:]
-options      = "v" 
-long_options = ["version"]
-version      = '1.3'
-
+import sys
 def main():
+  argumentList = sys.argv[1:]
+  options      = "v"
+  long_options = ["version"]
+  version      = '1.4'
   try:
     arguments, values = getopt.getopt(argumentList, options, long_options)
     for currentArgument, currentValue in arguments:
       if currentArgument in ("-v", "--version"):
         print ("pushit " + version)
+        break
   except:
     pass
-  if sys.argv[1] != "--version":
+  if len(sys.argv) == 1:
     commit_message    = input('Commit Comment: ')
     #working_directory = 'cd ~/mycode'
     working_directory = '/home/student/mycode'
     git_add           = 'git add *'
-    git_commit        = 'git commit -m "' + commit_message + '"' 
+    git_commit        = 'git commit -m "' + commit_message + '"'
     git_push          = 'git push origin'
     #os.system(working_directory)
     os.chdir(working_directory)
     os.system(git_add)
     os.system(git_commit)
-    os.system(git_commit)
     os.system(git_push)
 if __name__ == "__main__":
     main()
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
